48de410ee363a3c7b7a9c524e7ad39c349cb055a
Gakawarstone <pinnekerv@gmail.com>
2023-03-25 14:08:51 +0100

    feat: add iin validation

--- bot/handlers/user/register/get_iin.py
+++ bot/handlers/user/register/get_iin.py
@@ -12,6 +12,13 @@ class GetIinHandler(BaseHandler, OneTimeMessageHandlerExtension):
         await self.state.set_state(FSM.finish)
         await self.event.delete()
 
+        if not self._validate_iin(self.event.text):
+            self._set_one_time_message(
+                await self.event.answer('ИИН должен состоять из 12 цифр')
+            )
+            await self.state.set_state(FSM.get_iin)
+            return
+
         self.set(self.props.iin, self.event.text)
 
         await self.render_widget()
@@ -25,5 +32,10 @@ class GetIinHandler(BaseHandler, OneTimeMessageHandlerExtension):
         await self.state.set_state(FSM.get_phone_number)
 
     @staticmethod
-    def _validate_iin(self, iin: int) -> bool:
-        pass  # TODO:
+    def _validate_iin(iin: str) -> bool:
+        for ch in iin:
+            if not ch.isdigit():
+                return False
+        if len(iin) != 12:
+            return False
+        return True
