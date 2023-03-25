0f34d3e14c57ff588624ff4456eabb76d4d1cf21
Gakawarstone <pinnekerv@gmail.com>
2023-03-25 15:48:50 +0100

    feat: add if auth failed

--- bot/handlers/user/register/register.py
+++ bot/handlers/user/register/register.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from services.auth import AuthService
+from services.auth import AuthService, ApiRequestFailed
 from ..menu import show_menu
 from ._states import FSM
 from ._base import BaseHandler
@@ -9,9 +9,15 @@ from ._base import BaseHandler
 class RegisterHandler(BaseHandler):
     async def handle(self) -> Any:
         await self.state.set_state(FSM.finish)
-        user = AuthService.try_get_user(
-            iin=self.ctx.iin, phone_number=self.ctx.phone_number)
-        self.set(self.props.osi_user, user)
-        status_message = f'<u>Авторизовано:</u> <b>{user.fullname}</b> ✅'
-        await self.render_widget(status_message)
-        await show_menu(self.event, state=self.state)
+
+        try:
+            user = AuthService.try_get_user(
+                iin=self.ctx.iin, phone_number=self.ctx.phone_number)
+            self.set(self.props.osi_user, user)
+            status_message = f'<u>Авторизовано:</u> <b>{user.fullname}</b> ✅'
+            await self.render_widget(status_message)
+            await show_menu(self.event, state=self.state)
+        except ApiRequestFailed:
+            status_message = '<u>Проверьте корректность введенных данных</u> ❌'
+            await self.render_widget(status_message)
+            await self.event.answer('/register для повторной попытки')
--- bot/services/auth.py
+++ bot/services/auth.py
@@ -33,12 +33,16 @@ class OsiUserInfo:
     flats: list[Flat]
 
 
+class ApiRequestFailed(Exception):
+    """Failed to execute request"""
+
+
 class AuthService:
     @classmethod
     def try_get_user(cls, iin: int, phone_number: str) -> OsiUser:
         input_data = {'iin': str(iin), 'telephone': phone_number}
-        user_info = requests.post(API_URL + 'user/get/info/', json=input_data)
-        data = json.loads(user_info.text)
+        # user_info = requests.post(API_URL + 'user/get/info/', json=input_data)
+        data = cls._request_api('user/get/info/', input_data)
         return OsiUser(
             iin=data['iin'],
             phone_number=data['telephone'],
@@ -48,12 +52,23 @@ class AuthService:
     @classmethod
     def try_get_user_info(cls, user: OsiUser) -> OsiUserInfo:
         input_data = {'iin': user.iin, 'telephone': user.phone_number}
-        user_info = requests.post(API_URL + 'user/get/info/', json=input_data)
-        data = json.loads(user_info.text)
+        # user_info = requests.post(API_URL + 'user/get/info/', json=input_data)
+        data = cls._request_api('user/get/info/', input_data)
         return OsiUserInfo(
             user=user,
             flats=[cls._serialize_flat(flat) for flat in data['houses']]
         )
+
+    @classmethod
+    def _request_api(cls, endpoint: str, data: dict) -> dict:
+        try:
+            response = requests.post(API_URL + endpoint, json=data)
+            if response.status_code == 400:
+                raise ApiRequestFailed
+            return json.loads(response.text)
+        except Exception as e:
+            print(e)
+            raise ApiRequestFailed
 
     @classmethod
     def _serialize_flat(cls, data: dict) -> Flat:
